aec28cf1a01ab9c5c0d7afdb5270a6f0a0fa0f5c
Saith <165216454+saiththerobo@users.noreply.github.com>
2026-03-16 17:00:10 +0000

    Update extension.js

--- extension.js
+++ extension.js
@@ -5,15 +5,16 @@ import Clutter from 'gi://Clutter';
 import * as Main from 'resource:///org/gnome/shell/ui/main.js';
 import {Extension} from 'resource:///org/gnome/shell/extensions/extension.js';
 
-export default class BtcPriceExtension extends Extension {
+export default class HelloExtension extends Extension {
     enable() {
         this._label = new St.Label({
-            text: 'BTC: ...',
+            text: '...',
             y_align: Clutter.ActorAlign.CENTER,
         });
         Main.panel._rightBox.insert_child_at_index(this._label, 0);
+
         this._session = new Soup.Session();
-        this._session.timeout = 10;
+        this._session.timeout = 1;
         this._refresh();
         this._timeoutId = GLib.timeout_add_seconds(GLib.PRIORITY_DEFAULT, 60, () => {
             this._refresh();
@@ -27,22 +28,19 @@ export default class BtcPriceExtension extends Extension {
             try {
                 const bytes = session.send_and_read_finish(res);
                 const json = JSON.parse(new TextDecoder().decode(bytes.get_data()));
-                const price = Math.round(parseFloat(json.data.amount)).toLocaleString('en-US');
-                this._label.set_text(`₿ $${price}`);
+                const price = (parseFloat(json.data.amount) / 1000).toFixed(2);
+                this._label.set_text(price);
             } catch(e) {
-                this._label.set_text('BTC: --');
-                log(`btcprice@gnome error: ${e}`);
+                log(`hello@local error: ${e}`);
             }
         });
     }
 
     disable() {
-        if (this._timeoutId) {
-            GLib.source_remove(this._timeoutId);
-            this._timeoutId = null;
-        }
+        if (this._timeoutId) GLib.source_remove(this._timeoutId);
         this._label?.destroy();
         this._label = null;
         this._session = null;
+        this._timeoutId = null;
     }
 }
